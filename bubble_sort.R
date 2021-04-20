0cf9d58c99b7a4819a75d690fc8dae45b432e7ac
thanhngan2002 <73688091+thanhngan2002@users.noreply.github.com>
2021-04-20 10:18:36 +0700

    Add files via upload

--- bubble_sort.R
+++ bubble_sort.R
@@ -1,13 +1,31 @@
-example <- function(x){
-  n<-length(x)
-  for(j in 1:(n-1)){
-    for(i in 1:(n-j)){
-      if(x[i]>x[i+1]){
-        temp<-x[i]
-        x[i]<-x[i+1]
-        x[i+1]<-temp
+#example <- function(x){
+ # n<-length(x)
+  #for(j in 1:(n-1)){
+   # for(i in 1:(n-j)){
+    #  if(x[i]>x[i+1]){
+     #   temp<-x[i]
+      #  x[i]<-x[i+1]
+       # x[i+1]<-temp
+    #  }
+    #}
+  #}
+  #return(x)
+#}
+
+bubble_sort <- function(a){
+  i=1
+  repeat{
+    no_exchange=1
+    n= length(a)
+    for (j in n:2){
+      if (a[(j-1)]>a[j]){
+        temp=a[(j-1)]
+          a[j] = temp
+          no_exchange = 0
+        }
       }
-    }
+      i = i+1
+      if (no_exchange || j == (n-1)){break}
   }
-  return(x)
+  return (a)
 }
