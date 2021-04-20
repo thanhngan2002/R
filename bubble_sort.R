4250e331eca45fb21b65675b7d3a4c912d97e87f
thanhngan2002 <73688091+thanhngan2002@users.noreply.github.com>
2021-04-20 11:00:33 +0700

    Update bubble_sort.R

--- bubble_sort.R
+++ bubble_sort.R
@@ -1,16 +1,3 @@
-#example <- function(x){
- # n<-length(x)
-  #for(j in 1:(n-1)){
-   # for(i in 1:(n-j)){
-    #  if(x[i]>x[i+1]){
-     #   temp<-x[i]
-      #  x[i]<-x[i+1]
-       # x[i+1]<-temp
-    #  }
-    #}
-  #}
-  #return(x)
-#}
 
 bubble_sort <- function(a){
   i=1
